81a18a920c443b78836d7eccb2e7bd5bdb9f2a69
Steven Farrel <sfarrel@cern.ch>
2012-04-13 10:01:10 +0000

    make trigger matching run dependent to avoid d3pd errors

--- Root/SusyD3PDAna.cxx
+++ Root/SusyD3PDAna.cxx
@@ -166,6 +166,8 @@ void SusyD3PDAna::clearObjects()
 void SusyD3PDAna::matchElectronTriggers()
 {
   if(m_dbg) cout << "matchElectronTriggers" << endl;
+  int run = d3pd.evt.RunNumber();
+
   // loop over all baseline electrons
   for(uint i=0; i<m_baseElectrons.size(); i++){
     int iEl = m_baseElectrons[i];
@@ -176,15 +178,15 @@ void SusyD3PDAna::matchElectronTriggers()
 
     if(lv->Pt() > 25.*GeV){
       // e20_medium
-      if( matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e20_medium) ){
+      if( run < 186873 && matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e20_medium) ){
         flags |= TRIG_e20_medium;
       }
       // e22_medium
-      if( matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e22_medium) ){
+      if( run < 188902 && matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e22_medium) ){
         flags |= TRIG_e22_medium;
       }
       // e22vh_medium1
-      if( matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e22vh_medium1) ){
+      if( run > 188901 && matchElectronTrigger(lv->Eta(), lv->Phi(), d3pd.trig.trig_EF_el_EF_e22vh_medium1) ){
         flags |= TRIG_e22vh_medium1;
       }
     }
